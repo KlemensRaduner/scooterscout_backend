8aa18fbd7ed46504801e444d9dea125bb45b7d1e
klemens <klemens.raduner@nyp.ch>
2021-03-17 14:20:29 +0100

    spring initalizr

---
+++ src/main/java/ch/tbz/scooterscout/ScooterscoutApplication.java
@@ -0,0 +1,13 @@
+package ch.tbz.scooterscout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScooterscoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScooterscoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/tbz/scooterscout/ScooterscoutApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.tbz.scooterscout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScooterscoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
